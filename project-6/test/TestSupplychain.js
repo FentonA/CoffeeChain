317b9d3ed9305901efe3e0c38e9365999348bd74
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-04-27 15:31:47 +0200

    Implement consumer role

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -265,6 +265,7 @@ contract("SupplyChain", function (accounts) {
   // 8th Test
   it("Testing smart contract function purchaseItem() that allows a consumer to purchase coffee", async () => {
     const supplyChain = await SupplyChain.deployed();
+    await supplyChain.addConsumer(consumerID);
 
     // Declare and Initialize a variable for event
     var eventEmitted = false;

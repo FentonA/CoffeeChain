4a4d734a2fa5d7a3669dc5f28d61fd01d93f2e4c
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-05-01 13:07:42 +0200

    Replace truffle-contract.js file with actual truffle-contract module and adjust some changes in web3 usage

--- project-6/src/js/truffle-contract.js
+++ project-6/src/js/truffle-contract.js
@@ -25,7 +25,8 @@ var contract = (function(module) {
     return this.provider.sendAsync.apply(this.provider, arguments);
   };
 
-  var BigNumber = (new Web3()).toBigNumber(0).constructor;
+  console.log("Web 3 object", new Web3())
+  var BigNumber = (new Web3()).utils.toBN(0).constructor;
 
   var Utils = {
     is_object: function(val) {
